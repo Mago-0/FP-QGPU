12c875bad92e095486c03530cb95fa5155643e4b
Mago <st172378@stud.uni-stuttgart.de>
2026-03-23 13:02:52 +0100

    added dev pre-commit

--- src/fp_qgpu/pauli_matricies.py
+++ src/fp_qgpu/pauli_matricies.py
@@ -1,16 +1,20 @@
 import numpy as np
 
+
 def get_pauli_matricies():
     pauli_x = np.array([[0, 1], [1, 0]], dtype=complex)
     pauli_y = np.array([[0, -1j], [1j, 0]], dtype=complex)
     pauli_z = np.array([[1, 0], [0, -1]], dtype=complex)
     return pauli_x, pauli_y, pauli_z
 
+
 def pauli_x():
     return get_pauli_matricies()[0]
 
+
 def pauli_y():
     return get_pauli_matricies()[1]
+
 
 def pauli_z():
     return get_pauli_matricies()[2]

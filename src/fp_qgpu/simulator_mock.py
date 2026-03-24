412287309194fe106ddad8e6464621ca13eb1fa2
Mago <st172378@stud.uni-stuttgart.de>
2026-03-24 14:08:19 +0100

    added simulation_mock with function

---
+++ src/fp_qgpu/simulator_mock.py
@@ -0,0 +1,30 @@
+from qiskit import QuantumCircuit
+from qiskit_aer import AerSimulator
+from qiskit import transpile
+
+
+# einfacher Test-Circuit
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure_all()
+
+thisdict = {"qc": qc, "shots": 1024, "seed": 42}
+
+
+def simulator_mock(qc: QuantumCircuit, shots: int = 1024, seed: int | None = None):
+    simulator = AerSimulator(seed_simulator=seed)
+
+    # Transpile
+    qc.measure_all()
+    circ = transpile(qc, simulator)
+
+    # Run
+    result = simulator.run(circ, shots=shots).result()
+    counts = result.get_counts(circ)
+
+    return counts
+
+
+result = simulator_mock(**thisdict)
+print(result)
